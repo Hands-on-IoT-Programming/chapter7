a08cfbdcd07df37e409821df1c87b5a68c2d582e
Bernardo Ronquillo <bernardo.ronquillo@universaldx.com>
2022-04-08 17:52:54 +0200

    Remove authentication

--- nodered/settings.js
+++ nodered/settings.js
@@ -74,6 +74,7 @@ module.exports = {
     // -----------------
     // To password protect the Node-RED editor and admin API, the following
     // property can be used. See http://nodered.org/docs/security.html for details. 
+    /*
     adminAuth: {
         type: "credentials",
         users: [{
@@ -86,7 +87,7 @@ module.exports = {
             permissions: "read"
         }
     },
-
+    */
     // To password protect the node-defined HTTP endpoints (httpNodeRoot), or
     // the static content (httpStatic), the following properties can be used.
     // The pass field is a bcrypt hash of the password.
