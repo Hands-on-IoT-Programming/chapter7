eeabd186fc8fd205eca0ab9203c358f5dddf7f75
Bernardo Ronquillo <bernardo.ronquillo@universaldx.com>
2022-04-16 09:14:38 +0200

    Include pressure and humidity in the database

---
+++ nodered/startJsonDB_all.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Source ENV variables from .env file
+set -o allexport; source .env; set +o allexport
+echo 'The Pusher app key is ' $APP_KEY
+echo 'The Pusher cluster is ' $APP_CLUSTER
+
+# Start app
+npm run jsonDB_all
